51c8f8c6b1f4eb0b2b07b0188d09b91c8a5cb9d1
Sourabh Shrimali <shrimali.sourabh@yahoo.com>
2025-08-03 09:17:55 +0530

    create new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'login',
+        // loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent
     }
 ];
